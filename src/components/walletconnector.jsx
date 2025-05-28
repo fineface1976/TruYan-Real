1dc6a75f347ead8d127dd89245c838fa60254fa4
fineface1976 <50634139+fineface1976@users.noreply.github.com>
2025-05-28 11:41:15 +0100

    Update walletconnector.jsx

--- src/components/walletconnector.jsx
+++ src/components/walletconnector.jsx
@@ -1,1 +1,15 @@
+import Web3Modal from "web3modal";
+import Web3 from "web3";
 
+const connectWallet = async () => {
+  const web3Modal = new Web3Modal({
+    cacheProvider: true,
+    providerOptions: {}, // Add MAZOL Wallet config here
+  });
+  
+  const instance = await web3Modal.connect();
+  const web3 = new Web3(instance);
+  const accounts = await web3.eth.getAccounts();
+  
+  return accounts[0];
+};
