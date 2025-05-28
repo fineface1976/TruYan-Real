844f2670c46dc38f26ee6f16b3a1384f94b789c9
fineface1976 <50634139+fineface1976@users.noreply.github.com>
2025-05-28 15:29:06 +0100

    Update wallet.js

--- js/wallet.js
+++ js/wallet.js
@@ -1,1 +1,10 @@
 
+document.getElementById("connectWallet").addEventListener("click", async () => {
+  if (window.ethereum) {
+    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+    const shortAddress = `${accounts[0].slice(0, 6)}...${accounts[0].slice(-4)}`;
+    document.getElementById("walletAddress").textContent = shortAddress;
+  } else {
+    alert("Install MetaMask!");
+  }
+});
